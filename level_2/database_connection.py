c12f0d58fb3e69417ae72788b4cfba9cc0cd8967
Sahil-x-boop <sahiltalwar.official@gmail.com>
2025-10-28 12:35:16 +0530

    Apply .gitignore rules properly

--- level_2/database_connection.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def get_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Harman@123", 
-        database="inato_data"
-    )
